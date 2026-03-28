c3abda0613df16cf9b7dc079d939fb2fd37c31b6
VITORHUGORDM <vitrovitinho77@gmail.com>
2026-03-28 12:05:08 -0300

    chore: update API URL to production

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API_BASE_URL = "http://10.0.2.2:3001/api"; // Use localhost para emulador Android
+const API_BASE_URL = "https://api-farmacia-epbq.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
